f4802a1314eaf4c02cf1bfd151bb9cd473bbf487
kingsor <avventureweb@gmail.com>
2016-12-17 00:49:18 +0100

    add correct domain in settings.js

---
+++ settings.js
@@ -0,0 +1,30 @@
+exports.shorten_settings = {
+    dev_domain: 'localhost:8888', // Your development domain
+    live_domain: 'tinyur.herokuapp.com', // Live domain
+    dev_mongodb_uri: 'mongodb://localhost/shorten-node-dev',
+    live_mongodb_uri: '', // Leave blank to automatically pickup MONGOLAB_URI
+    sessionSecret: 'somekindarandomstringyoumakeup'
+};
+
+
+/*
+*   Schema definition
+*/
+var Schema = require('mongoose').Schema;
+// LinkStats is an embedded-document schema of LinkMaps
+var LinkStats = new Schema();
+LinkStats.add({
+    ip: String,
+    userAgent: String,
+    referrer: String,
+    timestamp: {type: Date, default: Date.now}
+});
+
+exports.models = {
+    LinkMaps: new Schema({
+        linkDestination: String,
+        linkHash: String,
+        timestamp: {type: Date, default: Date.now},
+        linkStats: [LinkStats]
+    })
+};
